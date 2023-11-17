1ea5efadb639d2a7e08e0e636269d0dc7eb44b17
Rodolfo do Nascimento Azevedo <rof20004@gmail.com>
2023-11-17 08:46:05 -0300

    fix(docker): corrigido execução do docker-compose

--- start-local-windows.bat
+++ start-local-windows.bat
@@ -2,5 +2,5 @@ docker-compose up -d
 echo "Waiting for API to start to run EF migrations..."
 pause 10
 echo "Running EF migrations..."
-docker container exec -it techchallenge-api-1 bash -c /app/efbundle
+docker container exec -it api1 bash -c /app/efbundle
 echo "Done running EF migrations."
